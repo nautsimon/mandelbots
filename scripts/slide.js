3ec8722f144235cae6d7c7c20554b1a059593eac
knolax <1339802534.kk@gmail.com>
2017-08-11 18:06:58 -0400

    fixed the slide system to use backgrounds on one element instead of selectively displaying multiple elements. added margins to biopics and changed the font of the site

--- scripts/slide.js
+++ scripts/slide.js
@@ -1,29 +1,31 @@
-slide = 0;
-i = 0;
-slidetimer = setInterval(next,2000);
-function upd() {
-	i = 0;
-	while (i < s) {
-		if (i == slide) {
-			document.getElementById("img" + i.toString()).style.display = "block";
-		} else {
-			document.getElementById("img" + i.toString()).style.display = "none"; 
-		}
-		i++;
-	}
+//everything's a global because i'm lazy and this is all the JS on the page anyways
+slidenum = 0; // number of slides
+slidelinks = []; //empty array for slide links
+slideid = ""; // slideshow id
+slide = 0; // current slide
+function setslideshow(rid, rslidenum,rslidelinks ) {
+	slideid = rid;
+	slidenum = rslidenum;
+	slidelinks = rslidelinks
+	slidetimer = setInterval(next,5000);
+	upd();
+}
+
+function upd() {	
+	document.getElementById(slideid).style.backgroundImage = "url('" + slidelinks[slide] + "')";
 	clearInterval(slidetimer);
-	slidetimer = setInterval(next,2000);
+	slidetimer = setInterval(next,5000);
 }
 function prev() {
 	if (slide > 0) {
 		slide--;
 	} else {
-		slide = s-1;
+		slide = slidenum-1;
 	}
 	upd();
 }
 function next() {
-	if (slide < s-1) {
+	if (slide < slidenum-1) {
 		slide++;
 	} else {
 		slide = 0;
