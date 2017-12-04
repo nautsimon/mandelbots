aaebc6b80bb9fd233e957158ceb8f18f65ef73c6
Simon mahns <ak.simonm@gmail.com>
2017-12-03 21:21:21 -0500

    pic update

--- scripts/slide.js
+++ scripts/slide.js
@@ -1,6 +1,6 @@
 //everything's a global because i'm lazy and this is all the JS on the page anyways
-slidenum = 0; // number of slides
-slidelinks = []; //empty array for slide links
+slidenum = 9; // number of slides
+slidelinks = ["pics/Picture1.png","pics/Picture2.jpg","pics/Picture3.jpg","pics/Picture4.png","pics/Picture5.png","pics/Picture6.jpg","pics/Picture7.jpg","pics/Capture.PNG","pics/Picture9.png"]; //empty array for slide links
 slideid = ""; // slideshow id
 slide = 0; // current slide
 function setslideshow(rid, rslidenum,rslidelinks ) {
